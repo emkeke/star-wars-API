b3b14d8b6b62e0b3e6ddceb1a5226cfcb63afd1b
Emma Käck <emma.keck@hotmail.com>
2022-04-22 15:44:38 +0200

    created API

--- src/services/StarWarsAPI.js
+++ src/services/StarWarsAPI.js
@@ -5,4 +5,31 @@
 
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://swapi.dev/api/'
+const BASE_URL = 'https://swapi.dev/api/'
+
+/**
+ * 
+ * Star Wars Films 
+ * Query & Pages
+ */
+const getFilms = async () => {
+    const res = await axios.get(`${BASE_URL}/films`)
+    return res.data
+}
+
+
+/**
+ * 
+ * Star Wars Characters 
+ * Query & Pages
+ */
+ const getPeople = async () => {
+    const res = await axios.get(`${BASE_URL}/people`)
+    return res.data
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    getFilms,
+    getPeople
+}
