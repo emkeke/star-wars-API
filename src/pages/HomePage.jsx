801cfef06d42cb741af176bd401b4187e9698593
Emma Käck <emma.keck@hotmail.com>
2022-04-25 13:52:09 +0200

    Image to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
-import { Link } from 'react-router-dom'
+import StarWarsImg from '../assets/images/star-wars-front-img.jpeg'
+import Row from 'react-bootstrap/Row'
+import Image from 'react-bootstrap/Image'
+
 
 const HomePage = () => {
 	return (
 		<>
 			<h1 className='text-center'>Star Wars Wikipedia</h1>
+
+			<Row className="justify-content-md-center">
+       			<Image src={StarWarsImg} fluid />
+			</Row>
 		</>
 	)
 }
